9ebf714520cbf41c9a253661ca5eb5d9b278344e
pachamamista <31214228+pachamamista@users.noreply.github.com>
2018-09-18 18:14:07 -0400

    y

--- day-and-night_week5-class/sketch.js
+++ day-and-night_week5-class/sketch.js
@@ -6,13 +6,17 @@ var r = 255;
 var g = 255;
 var b = 255;
 
-var cX = 50;
-var cY = 50;
+var cX = 100;
+var cY = 250;
 var cEdge = 20;
 
 var rectX = 50;
 var rectY = 75;
 var rectSize = 100;
+
+var sunShow = false;
+
+var sunButton;
 
 function preload(){
 //preload goes above setup and will be used and loaded before project starts
@@ -20,7 +24,6 @@ function preload(){
 //find the image and keep it there til I want to use it
 
 sun = loadImage('assets/sun.png');
-//use png because you can use transparency
 moon = loadImage('assets/moon.png');
 
 }
@@ -28,20 +31,51 @@ moon = loadImage('assets/moon.png');
 function setup() {
   // put setup code here
   createCanvas(400,400);
+  sunButton = createButton("Press to show/hide sun");
+  sunButton.position(500,100);
+  sunButton.mousePresssed(function(){
+    if(sunShow == false){
+      sunShow = true;
+    }else {
+      sunShow = false;
+    }
+  });
 }
 
-function draw() {
-
+function draw(){
+background(0);
   fill(r,g,b);
   rect(rectX,rectY,rectSize,rectSize);
+  imageMode(CENTER);
+  image(sun,cX,cY,sun.width/4,sun.height/4);
+
+if(sunShow == true){
+  image(sun,300,200);
+}
 }
 
-function mousePresssed(){
 
+function mousePresssed(){
+  
   if(mouseX > rectX && mouseX < rectX + rectSize && mouseY > rectY && mouseY < rectY + rectSize){
-//    console.log("DOES IT WORK");
+//console.log("DOES IT WORK");
     r = random(0,256);
     g = random(0,256);
     b = random(0,256);
 }
+
+var sunDist = dist(mouseX,mouseY,cX,cY);
+console.log("Sun Dist: " + sunDist);
+if(sunDist < 40){
+  //what we are doing when we click on this is show/hide sun image
+if(sunShow == false){
+  sunShow = true;
+}else {
+  sunShow = false;
 }
+  console.log(sunShow);
+
+}
+}
+
+//end of code
