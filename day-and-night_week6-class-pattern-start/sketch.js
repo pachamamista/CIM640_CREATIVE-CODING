1a1ac5e583d375b0789f60b924d3aca335cd3155
pachamamista <31214228+pachamamista@users.noreply.github.com>
2018-09-20 17:26:51 -0400

    y

---
+++ day-and-night_week6-class-pattern-start/sketch.js
@@ -0,0 +1,21 @@
+
+var curImage,sun,moon;
+
+function preload(){
+  sun=loadImage("assets/sun.png");
+  moon=loadImage("assets/moon.png");
+
+}
+
+
+function setup() {
+  // put setup code here
+createCanvas(400,400);
+}
+
+function draw() {
+image(sun,10,10);
+}
+
+
+//End of code
